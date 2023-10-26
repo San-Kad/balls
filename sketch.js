0370e29f29eaeb81a44d5f8adffafc994ce5ed07
San-Kad <131393677+San-Kad@users.noreply.github.com>
2023-10-26 18:34:40 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,139 +1,123 @@
 const Engine = Matter.Engine;
-const World = Matter.World;
+const World= Matter.World;
 const Bodies = Matter.Bodies;
-const Constraint = Matter.Constraint;
 
 var engine, world;
-var canvas;
-var palyer, playerBase, playerArcher;
-var playerArrows = [];
 
+var particle1, particle2,particle3,particle4,particle5;
+var particle6, particle7,particle8,particle9,particle10;
+var rotator1, rotator2, rotator3;
+var block1, block2;
 
-function preload() {
-  backgroundImg = loadImage("./assets/background.png");
-  baseimage = loadImage("./assets/base.png");
-  playerimage = loadImage("./assets/player.png");
-}
+var angle1=60;
+var angle2=60;
+var angle3=60;
 
-function setup() {
-  canvas = createCanvas(windowWidth, windowHeight);
+function setup(){
+    var canvas = createCanvas(550,600);
+    engine = Engine.create();
+    world = engine.world;
 
-  engine = Engine.create();
-  world = engine.world;
-
-  angleMode(DEGREES);
-
-  var options = {
-    isStatic: true
-  };
-
-  playerBase = Bodies.rectangle(200, 350, 180, 150, options);
-  World.add(world, playerBase);
-
-  player = Bodies.rectangle(250, playerBase.position.y - 160, 50, 180, options);
-  World.add(world,player)
-
-  playerArcher = new PlayerArcher(
-    340,
-    playerBase.position.y - 112,
-    120,
-    120
-  );
-}
-
-function draw() {
-  background(backgroundImg);
-
-  Engine.update(engine);
-  image(baseimage,playerBase.position.x,playerBase.position.y,180,150)
-  image(playerimage,player.position.x,player.position.y,50,180)
-
-  playerArcher.display();
-
-   // for (var i; i < playerArrows.length; i++) {
-   //   if (playerArrows[i] !== undefined) {
-   //     playerArrows[i].display();
-   //   }
-   // }
-
-   // for (var i = 0, i++) {
-   //   if (playerArrows[i] !== undefined) {
-   //     playerArrows[i].display();
-   //   }
-   // }
-  
-   for (var i = 0; i < playerArrows.length; i++) {
-     if (playerArrows[i] !== undefined) {
-       playerArrows[i].display();
-     }
-   }
-
-   // for (var i) {
-   //   if (playerArrows[i] !== undefined) {
-   //     playerArrows[i].display();
-   //   }
-   // }
-
-  // Title
-  fill("#FFFF");
-  textAlign("center");
-  textSize(40);
-  text("EPIC ARCHERY", width / 2, 100);
-}
-
-// function keyPressed() {
-//   if (keyCode === 32) {
-//     var posX = playerArcher.body.position.x;
-//     var posY = playerArcher.body.position.y;
-//     var angle = playerArcher.body.angle;
-//     var arrow = new PlayerArrow(posX, posY, 100, 10, angle);
-
-//     Matter.Body.setAngle(arrow.body, angle);
-//     playerArrows.push(arrow);
-//   }
-// }
-
-// function keyPressed() {
-//   if (keyCode === 23) {
-//     var posX = playerArcher.body.position.x;
-//     var posY = playerArcher.body.position.y;
-//     var angle = playerArcher.body.angle;
-//     var arrow = new PlayerArrow(posX, posY, 100, 10, angle);
-
-//     Matter.Body.setAngle(arrow.body, angle);
-//     playerArrows.push(arrow);
-//   }
-// }
-
-// function keyPressed() {
-//   if (keyCode === 32) {
-//     var posX = playerArcher.body.position.x;
-//     var posY = playerArcher.body.position.y;
-//     var angle = playerArcher.body.angle;
-//     var arrow = new PlayerArrow(posX, posY, 100, 10, angle);
-
-//     Matter.Body.setAngle(arrow.body, angle);
-//     playerArrows.push(arrow);
-//   }
-// }
-
-// function keyPressed() {
-//   if (keyCode === 32) {
-//     var posX = playerArcher.body.position.x;
-//     var posY = playerArcher.body.position.y;
-//     var angle = playerArcher.body.angle;
-//     var arrow = new PlayerArrow(posX, posY, 100, 10, angle);
-
-//     Matter.Body.setAngle(arrow.body, angle);
-//     playerArrows(arrow);
-//   }
-// }
-
-function keyReleased() {
-  if (keyCode === 32) {
-    if (playerArrows.length) {
-      var angle = playerArcher.body.angle;
-      playerArrows[playerArrows.length - 1].shoot(angle);
+    //created plane and block bodies
+    var plane_options={
+      isStatic: true
     }
-  }
+
+    plane = Bodies.rectangle(600,height,1200,20,plane_options);
+    World.add(world,plane);
+    block1=Bodies.rectangle(100,400,150,20,plane_options);
+    World.add(world,block1);
+    block2=Bodies.rectangle(400,400,150,20,plane_options);
+    World.add(world,block2);
+
+    //created multiple of particle bodies 
+    var particle_options = {
+      restitution:0.4,
+      friction:0.02
+    }
+
+    particle1 = Bodies.circle(220,10,10,particle_options);
+    World.add(world,particle1);
+    particle2 = Bodies.circle(220,10,10,particle_options);
+    World.add(world,particle2);
+    particle3 = Bodies.circle(225,10,10,particle_options);
+    World.add(world,particle3);
+    particle4 = Bodies.circle(230,10,10,particle_options);
+    World.add(world,particle4);
+    particle5 =Bodies.circle(230,10,10,particle_options);
+    World.add(world,particle5);
+
+    var rotator_options={
+       //isStatic=true
+      // isStatic true
+      // isStatic:false
+       isStatic:true
+    };
+
+    // rotator1 = rectangle(250,200,150,20,rotator_options);
+    // World.add(world,rotator1);
+
+    // rotator1 = Bodies.rectangle();
+    // World.add(world,rotator1);
+
+    // rotator1 = Bodies.circle(250,200,150,20,rotator_options);
+    // World.add(world,rotator1);
+
+     rotator1 = Bodies.rectangle(250,200,150,20,rotator_options);
+     World.add(world,rotator1);
+
+    rotator2 = Bodies.rectangle(250,200,150,20,rotator_options);
+    World.add(world,rotator2);
+
+    rotator3 = Bodies.rectangle(250,200,150,20,rotator_options);
+    World.add(world,rotator3);
+
+    //styling the bodies here
+    fill("brown");
+    rectMode(CENTER);
+    ellipseMode(RADIUS);
+
+}
+
+function draw(){
+    background("lightgreen");
+    Engine.update(engine);
+
+  //created shape for plane and stand
+  rect(plane.position.x,plane.position.y,1200,20);
+  rect(block1.position.x,block1.position.y,150,20);
+  rect(block2.position.x,block2.position.y,150,20);
+
+  //created shape for all the paticles
+  ellipse(particle1.position.x,particle1.position.y,10);
+  ellipse(particle2.position.x,particle2.position.y,10);
+  ellipse(particle3.position.x,particle3.position.y,10);
+  ellipse(particle4.position.x,particle4.position.y,10);
+  ellipse(particle5.position.x,particle5.position.y,10);
+
+  //created shape for all the rotators
+  Matter.Body.rotate(rotator1,angle1)
+  push();
+  translate(rotator1.position.x,rotator1.position.y);
+  rotate(angle1);
+  rect(0,0,150,20);
+  pop();
+  angle1 +=0.2;
+
+  Matter.Body.rotate(rotator2,angle2)
+  push();
+  translate(rotator2.position.x,rotator2.position.y);
+  rotate(angle2);
+  rect(0,0,150,20);
+  pop();
+  angle2 +=0.3;
+
+  Matter.Body.rotate(rotator3,angle3)
+  push();
+  translate(rotator3.position.x,rotator3.position.y);
+  rotate(angle3);
+  rect(0,0,150,20);
+  pop();
+  angle3 +=0.4;
+    
 }
